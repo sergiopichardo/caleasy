267050298886e60162e3fdb59850a7505c6ce49f
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-02-17 17:36:01 -0600

    feat(events): list upcoming events

--- app/main.py
+++ app/main.py
@@ -1,42 +1,17 @@
-import datetime
-from googleapiclient.errors import HttpError
-
+from services.events.event import EventService
 from services.authentication.auth import AuthService
 
 def main():
     
     try:
         auth_service = AuthService()
-        service = auth_service.get_authenticated_service()
+        event_service = EventService(auth_service)
 
-        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
+        event_service.list_upcoming_events(5)
 
-        if not events:
-            print("No upcoming events found.")
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-        
-    except HttpError as error:
+    except Exception as error:
         print(f"An error occurred: {error}")
         
 
 if __name__ == "__main__":
     main()
-    
--- app/services/events/event.py
+++ app/services/events/event.py
@@ -1,5 +1,5 @@
 import datetime 
-from googleapiclient.error import HttpError
+from googleapiclient.errors import HttpError
 from services.authentication.auth import AuthService
 
 class EventService:
@@ -12,3 +12,35 @@ class EventService:
         :param auth_service: An instance of AuthService for authenticated Google API operations.
         """
         self.service = auth_service.get_authenticated_service()
+
+    def list_upcoming_events(self, num_events=10):
+        """
+        Lists the upcoming events from the user's primary Google Calendar.
+
+        Prints the start time and summary of each upcoming event to the console. If no upcoming events are found,
+        a message indicating this will be printed.
+
+        :param num_events: The maximum number of events to retrieve (default is 10).
+        """
+        try: 
+            now = datetime.datetime.utcnow().isoformat() + "Z" # 'Z' indicates UTC time
+            print("getting the upcoming events")
+            events_result = self.service.events().list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=num_events,
+                singleEvents=True,
+                orderBy="startTime"
+            ).execute()
+
+            events = events_result.get("items", [])
+
+            if not events:
+                print("No upcoming events found.")
+            else: 
+                for event in events:
+                    start = event["start"].get("dateTime", event["start"].get("date"))
+                    print(start, event.get("summary"))
+
+        except HttpError as error:
+            print(f"An error ocurred listing upcoming events: {error}")
