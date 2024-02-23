661749f02394831e94488f56f2a3e1da3720a696
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-02-22 19:51:12 -0600

    chore: remove comments

--- services/calendar_service.py
+++ services/calendar_service.py
@@ -1,17 +1,14 @@
 from services.auth_service import AuthService
 
 
+from googleapiclient.errors import HttpError
 
 
-# from googleapiclient.errors import HttpError
-# from auth import AuthService
+class CalendarService:
+    def __init__(self, auth_service: AuthService) -> None:
+        self.service = auth_service.get_authenticated_service()
 
-
-# class CalendarService:
-#     def __init__(self, auth_service: AuthService) -> None:
-#         self.service = auth_service.get_authenticated_service()
-
-#     def list_all_calendars(self):
-#         events_result = self.service.calendarList().list().execute()
-#         calendars = events_result.get("items", [])
-#         return calendars 
+    def list_all_calendars(self):
+        events_result = self.service.calendarList().list().execute()
+        calendars = events_result.get("items", [])
+        return calendars 
