c531073aacb9fc6f7f5f5616a95f577f36d15fa8
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-02-22 19:50:44 -0600

    feat(events): import events app into main program

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-from services.auth_service import AuthService
-# from event import EventService
-from calendar import CalendarService
+import typer 
+from cli.events import app as events_app
 
-def main():
-    
-    try:
-        auth_service = AuthService()
-        # event_service = EventService(auth_service)
-        calendar_service = CalendarService(auth_service)
+app = typer.Typer()
+app.add_typer(events_app, name="events")
 
-        calendars = calendar_service.list_all_calendars()
-        
-        for calendar in calendars:
-            print(calendar, '\n')
-            if calendar['primary']:
-                print(f"[PRIMARY] {calendar['summary']}")
-            else:
-                print(calendar['summary'])
-
-    except Exception as error:
-        print(f"An error occurred: {error}")
-        
 
 if __name__ == "__main__":
-    main()
+    app()
